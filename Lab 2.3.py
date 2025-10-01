7e13a5eec619d5c5d0e4a2c90d306dd8f5c5031b
CynIsAfk <travis.r.coffey@gmail.com>
2025-10-01 01:22:43 -0400

    Working, Finished

--- Lab 2.3.py
+++ Lab 2.3.py
@@ -2,7 +2,7 @@ import os
 os.system("cls")
 
 print("Task #1")
-full_name = input("Enter Full Name:")
+full_name = input("Enter Full Name: ")
 space_Location = full_name.find(" ",0)
 first_name = full_name[:space_Location]
 last_name = full_name[(space_Location+1):]
@@ -25,3 +25,33 @@ phrase_part1 = star_wars[0:phrase_split]
 phrase_part2 = star_wars[phrase_split:2*phrase_split]
 phrase_part3 = star_wars[2*phrase_split:3*phrase_split]
 print(phrase_part2,phrase_part3,phrase_part1,sep="\n")
+print("Task #4")
+number = input("Enter 5 digit number: ")
+number_1 = int(number[0])
+number_2 = int(number[1])
+number_3 = int(number[2])
+number_4 = int(number[3])
+number_5 = int(number[4])
+number_total = number_1 + number_2 + number_3 + number_4 + number_5
+print(number_total)
+print("Task #5")
+funny_phrase = "Why, you stuck-up half-witted scruffy-looking nerf herder."
+print(funny_phrase[0:57:2])
+print(funny_phrase[-57::2])
+print("Task #6")
+from datetime import date
+
+
+today = date.today()
+
+today = today.strftime("%Y,%B,%d")
+
+
+print(f"The date today is {today}")
+
+date_length = int(len(today))
+month_length = int(date_length-8)
+year = today[0:4]
+month = today[5:month_length+5]
+day = today[-2:8+month_length]
+print(f"It is the {day} of {month}, {year}")
