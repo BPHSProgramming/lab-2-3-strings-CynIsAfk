e38f4cf031a738f6b37601b86752db80787a26c5
CynIsAfk <travis.r.coffey@gmail.com>
2025-10-02 13:34:15 -0400

    Working

--- Lab 2.3.py
+++ Lab 2.3.py
@@ -8,7 +8,7 @@ space_Location = full_name.find(" ",0)
 first_name = full_name[:space_Location]
 last_name = full_name[(space_Location+1):]
 print(first_name.title(),last_name.title())
-print(first_name[0].lower(),first_name[1:].upper()," ",last_name[0].lower(),last_name[1:].upper(),sep="")
+print(f"{first_name[0].lower()}{first_name[1:].upper()} {last_name[0].lower()}{last_name[1:].upper()}")
 
 print("Task #2")
 star_wars = "Once you start down the dark path, forever will it dominate your destiny."
@@ -23,7 +23,6 @@ print(encryption)
 print("Task #3")
 original_length = len(star_wars)
 phrase_split = ((original_length-1)//3)
-print(phrase_split)
 phrase_part1 = star_wars[0:phrase_split]
 phrase_part2 = star_wars[phrase_split:2*phrase_split]
 phrase_part3 = star_wars[2*phrase_split:3*phrase_split]
@@ -36,14 +35,14 @@ number_2 = number[1]
 number_3 = number[2]
 number_4 = number[3]
 number_5 = number[4]
-number_total = int(number_1 + number_2 + number_3 + number_4 + number_5)
+number_total = int(number_1) + int(number_2) + int(number_3) + int(number_4) + int(number_5)
 print(f"{number_1} + {number_2} + {number_3} + {number_4} + {number_5} = {number_total}")
 
 print("Task #5")
 funny_phrase = "Why, you stuck-up half-witted scruffy-looking nerf herder."
-phrase_slice = funny_phrase.find(".")
+phrase_slice = len(funny_phrase)-1
 print(funny_phrase[0:int(phrase_slice):2])
-print(funny_phrase[-int(phrase_slice)::2])
+print(funny_phrase[-1*(int(phrase_slice))::2])
 
 print("Task #6")
 from datetime import date
@@ -56,9 +55,9 @@ today = today.strftime("%Y,%B,%d")
 
 print(f"The date today is {today}")
 
-date_length = int(len(today))
-month_length = int(date_length-8)
-year = today[0:4]
-month = today[5:month_length+5]
-day = today[-2:8+month_length]
+date_length = int(len(today)-1)
+year = today[0:today.find(",",0)]
+month = today[today.find(",",0)+1:today.find(",",today.find(",",0)+1)]
+day = today[today.find(",",today.find(",",today.find(",",0))+1)+1:]
+
 print(f"It is the {day} of {month}, {year}")
